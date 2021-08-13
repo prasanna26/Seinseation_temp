b80a431b337834c2b0f2463cf0c8c040cc66c411
Prasanna <prasanna.sachin@yahoo.co.in>
2021-08-13 13:15:37 -0400

    Adding support for resized images in the Mobile view

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Sidebar from './components/sidebar'
 import Introduction from './components/introduction'
 import About from './components/about'
-import Timeline from './components/timeline'
+// import Timeline from './components/timeline'
 
 class App extends Component {
   render() {
@@ -11,11 +11,11 @@ class App extends Component {
       <div id="colorlib-page">
         <div id="container-wrap">
          	<Sidebar></Sidebar>
-				<div id="colorlib-main">
-					<Introduction></Introduction>
-					<About></About>
-					<Timeline></Timeline>
-          	</div>
+          <div id="colorlib-main">
+            <Introduction></Introduction>
+            <About></About>
+            {/* <Timeline></Timeline> */}
+          </div>
       	</div>
       </div>
     );
--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -1,51 +1,82 @@
-import React, { Component } from 'react'
+import React, { Component,useState, useEffect } from 'react';
+// import seinesationDesktopImage from 'images/Seinesation_resized.jpeg';
 
-export default class Introduction extends Component {
-  render() {
+const Introduction = ()=>{
+  
+    const imagestyle ={
+      backgroundImage : 'url(images/Seinesation_resized.jpeg)',
+      // color : "white",
+    };
+    const fontColorStyel ={
+      color:"white"
+    };
+    let seinesationDesktopImageUrl = 'url(images/Seinesation_resized.jpeg)';
+    let seinesationMobileImageUrl = 'url(images/Seinesation_mob.jpeg)';
+    let cleoDesktopImageUrl = 'url(images/cleo_desktop.jpg)';
+    let uncertaintyDesktopImageUrl = 'url(images/Uncertainty_desktop.jpg)';
+    let cleoMobileImageUrl = 'url(images/cleo_mob.jpeg)';
+    let uncertaintyMobileImageUrl = 'url(images/Uncertainty_mob.jpeg)';
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+    const seinesationImageUrl = windowWidth >=650 ? seinesationDesktopImageUrl : seinesationMobileImageUrl ;
+
+    const cleoImageUrl = windowWidth >=650 ? cleoDesktopImageUrl : cleoMobileImageUrl ;
+    const uncertaintyImageUrl = windowWidth >=650 ? uncertaintyDesktopImageUrl : uncertaintyMobileImageUrl ;
+
+    useEffect(() => {
+        const handleWindowResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+        
+        window.addEventListener('resize', handleWindowResize);
+
+        return () => {
+            window.removeEventListener('resize', handleWindowResize);
+        }
+    }, []);
     return (
       <div>
         <section id="colorlib-hero" className="js-fullheight" data-section="home">
           <div className="flexslider js-fullheight">
             <ul className="slides">
-              <li style={{backgroundImage: 'url(images/img_bg.jpg)'}}>
+              <li style={{backgroundImage : seinesationImageUrl}}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner js-fullheight">
                         <div className="desc">
-                          <h1>Hi! <br />I'm Dhruv</h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://docs.google.com/document/d/15coSa2rnZcdywihM-pWTvoGqE65JAkxQAh6JDBNrF2Q/edit?usp=sharing" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
+                          <h1 style={fontColorStyel}>Hi! <br />I'm Seinesation</h1>
+                          <p style={fontColorStyel}><a className="btn btn-primary btn-learn" href="#about" target="_blank" rel="noopener noreferrer" style ={{color:"white",border:"white",borderColor:"white"}}>Groove to My Music<i className="icon-download4" /></a></p>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </li>
-              <li style={{backgroundImage: 'url(images/img_bg.jpg)'}}>
+              <li style={{backgroundImage: cleoImageUrl}}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner">
                         <div className="desc">
-                          <h1>I love building<br /> THINGS !!</h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://github.com/dbarochiya" target="_blank" rel="noopener noreferrer">View Projects <i className="icon-briefcase3" /></a></p>
+                          <h1 style={fontColorStyel}><br />  </h1>
+                          {/* <p><a className="btn btn-primary btn-learn" href="https://github.com/dbarochiya" target="_blank" rel="noopener noreferrer">View Projects <i className="icon-briefcase3" /></a></p> */}
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </li>
-              <li style={{backgroundImage: 'url(images/img_bg.jpg)'}}>
+              <li style={{backgroundImage: uncertaintyImageUrl}}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner">
                         <div className="desc">
-                          <h1>I often <br/>Write ... </h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://dhruvbarochiya.me/" target="_blank" rel="noopener noreferrer">View Blog <i className="icon-book" /></a></p>
+                          {/* <h1>I often <br/>Write ... </h1> */}
+                          {/* <p><a className="btn btn-primary btn-learn" href="https://dhruvbarochiya.me/" target="_blank" rel="noopener noreferrer">View Blog <i className="icon-book" /></a></p> */}
                         </div>
                       </div>
                     </div>
@@ -57,5 +88,7 @@ export default class Introduction extends Component {
         </section>
       </div>
     )
-  }
-}
+  
+};
+
+export default Introduction;
