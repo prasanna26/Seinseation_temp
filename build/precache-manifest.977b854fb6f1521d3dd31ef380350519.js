0122163d808c9b29353c30b802ad06afe823a36f
prasanna26 <prasanna26.sachin@gmail.com>
2022-03-05 11:07:32 -0500

    Adding build folder

---
+++ build/precache-manifest.977b854fb6f1521d3dd31ef380350519.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b44a4770bb671b5cbf62",
+    "url": "./static/css/main.39b50f81.chunk.css"
+  },
+  {
+    "revision": "b44a4770bb671b5cbf62",
+    "url": "./static/js/main.b44a4770.chunk.js"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "3f52977d65da2725e35c",
+    "url": "./static/js/2.3f52977d.chunk.js"
+  },
+  {
+    "revision": "5117e33665e385bb271e216b85c63f59",
+    "url": "./index.html"
+  }
+];
